23ab1cf058214195bff970e7fac45591f847e93e
5730 - Aryamol U <5730aru@manorama.net>
2025-10-08 15:30:37 +0530

    navbar alignment changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
 
   return (
     <nav className="bg-white shadow-md fixed w-full top-0 left-0 z-50">
-      <div className="max-w-7xl mx-auto px-6 py-3 flex items-center justify-between">
-        {/* Logo & Title */}
+      <div className=" px-6 py-3 flex items-center justify-between">
+        {/* Left: Logo & Title */}
         <div className="flex items-center space-x-3">
           <img
             src="/logo.png" // place your logo in public folder
@@ -14,12 +14,12 @@ const Navbar = () => {
             className="w-10 h-10"
           />
           <h1 className="text-xl font-bold text-gray-900">
-            Bhoshini <span className="text-black font-semibold"></span>
+            Bhoshini
           </h1>
         </div>
 
-        {/* Desktop Menu */}
-        <div className="hidden md:flex space-x-6 text-gray-700 font-medium">
+        {/* Right: Menu Items */}
+        <div className="hidden md:flex items-center space-x-6 text-gray-700 font-medium">
           <a href="/" className="hover:text-black">
             Home
           </a>
@@ -39,10 +39,7 @@ const Navbar = () => {
             Sign In
           </a>
         </div>
-
       </div>
-
-      
     </nav>
   );
 };
